515c9f65bbba23be6abc0ea5b6f1caadf3c024c1
adita111 <adita1adita@yahoo.com>
2025-04-02 17:10:57 +0300

    inainte de 88

--- server.js
+++ server.js
@@ -6,19 +6,44 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD,
-);
-
+dotenv.config(); // Load .env variables
 mongoose
-  .connect(DB, {
-    userNewUrlParser: true,
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false,
   })
-  .then(() => {
-    console.log('DB connection successful!');
+  .then(() => console.log('✅ DB connection successful!'))
+  .catch((err) => console.error('❌ DB connection error:', err));
+
+const tourScheme = new mongoose.Schema({
+  name: {
+    type: String,
+    requierd: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourScheme);
+
+const testTour = new Tour({
+  name: 'The Park camper',
+  price: 997,
+  rating: 4.7,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error:', err);
   });
 
 const port = process.env.PORT || 8000;
