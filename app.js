f148e2a6f41be6caffab2b2ae7616ef0c3b8ae68
adita111 <adita1adita@yahoo.com>
2025-04-11 12:08:06 +0300

    dupa cum sa ai grija de erori

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const app = express();
-
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routs/tourRoutes');
 const userRouter = require('./routs/userRoutes');
 //////Middlewares
@@ -15,16 +16,24 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // const err = new Error(
+  //   `Cant find ${req.originalUrl} on this server`,
+  // );
+  // err.statusCode = 404;
+  // err.status = 'fail';
+  next(
+    new AppError(`Cant find ${req.originalUrl} on this server`, 404),
+  );
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('Uncought Exception');
+  console.log(err.name, err.mesage);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -14,10 +20,18 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => console.log('✅ DB connection successful!'))
-  .catch((err) => console.error('❌ DB connection error:', err));
+  .catch((err) => console.log('❌ DB connection error:', err));
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('Unhandle Rejection');
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
 });
